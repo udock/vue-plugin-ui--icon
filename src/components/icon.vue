2c164166080e8aa7ea9f58f7bca8e1fcaf7d9d98
vaniship <vaniship@gmail.com>
2019-01-05 10:03:40 +0800

    fix: fix build tools config

--- src/components/icon.vue
+++ src/components/icon.vue
@@ -2,38 +2,38 @@
   <i :class="classes" :style="styles"></i>
 </template>
 <script>
-  // import fontList from '../fonts'
-  const prefixCls = 'u-icon'
+// import fontList from '../fonts'
+const prefixCls = 'u-icon'
 
-  export default {
-    name: 'UIcon',
-    props: {
-      type: String,
-      size: [Number, String],
-      color: String
+export default {
+  name: 'UIcon',
+  props: {
+    type: String,
+    size: [Number, String],
+    color: String
+  },
+  computed: {
+    classes () {
+      return `${prefixCls} ${prefixCls}-${this.type}`
     },
-    computed: {
-      classes () {
-        return `${prefixCls} ${prefixCls}-${this.type}`
-      },
-      // fontStr () {
-      //   // html &#xf355 js \x
-      //   // return String.fromCharCode(fontList[this.type])
-      //   return fontList[this.type]
-      // },
-      styles () {
-        let style = {}
+    // fontStr () {
+    //   // html &#xf355 js \x
+    //   // return String.fromCharCode(fontList[this.type])
+    //   return fontList[this.type]
+    // },
+    styles () {
+      let style = {}
 
-        if (this.size) {
-          style['font-size'] = `${this.size}px`
-        }
-
-        if (this.color) {
-          style.color = this.color
-        }
-
-        return style
+      if (this.size) {
+        style['font-size'] = `${this.size}px`
       }
+
+      if (this.color) {
+        style.color = this.color
+      }
+
+      return style
     }
   }
+}
 </script>
